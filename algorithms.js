b00bf6869f1197e0c3f7fe81c724595247f6bd3a
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-07-13 12:41:23 +0200

    Adding fibonacci algorithm

---
+++ algorithms.js
@@ -0,0 +1,19 @@
+/*
+    Big O Guide
+    Calculation not dependent on input size - O(1)
+    Linear - O(n) - 1 loop
+    Quadratic - O(n^2) - 2 nested loops
+    Input size reduced by half - 0(logn)
+
+*/
+
+//Big O - O(n)
+const fibonacci = (n) => {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+};
+
+module.exports = { fibonacci };
---
+++ index.js
@@ -0,0 +1,7 @@
+const { fibonacci } = require("./algorithms");
+
+/*
+console.log(fibonacci(2)); // [0,1]
+console.log(fibonacci(3)); // [0,1,1]
+console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
+*/
